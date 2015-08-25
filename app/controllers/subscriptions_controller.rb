4695facdc2646460376eaaa08c7b73cd1e88e4df
Maciej Nowak <maciej.nowak90@gmail.com>
2015-08-25 19:05:24 +0200

    Implement Subscriptions#create action along with its tests

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -3,5 +3,11 @@ class SubscriptionsController < ApplicationController
   end
 
   def create
+    @subscription = Subscription.new(email: params[:email])
+    if @subscription.save
+      render json: { email: params[:email] }, status: :created
+    else
+      render nothing: true, status: :unprocessable_entity
+    end
   end
 end
--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe SubscriptionsController, :type => :controller do
+  describe "POST create" do
+    it "returns 201 with email when subscription was created" do
+      post :create, email: "maciejnowak@gmail.com"
+
+      expect(response.code).to eq("201")
+      expect(JSON.parse(response.body)).to eq({ "email" => "maciejnowak@gmail.com" })
+    end
+
+    it "returns 422 with no content when record could not be saved" do
+      post :create, email: "maciejnowak.gmail.com"
+
+      expect(response.code).to eq("422")
+      expect(response.body).to be_empty
+    end
+  end
 end
