7df8db9e6f6692eb88805456588d002e1a7aa456
Maciej Nowak <maciej.nowak90@gmail.com>
2015-08-25 17:05:29 +0200

    Initialize SubscriptionsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'subscriptions/index'
+
+  get 'subscriptions/create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
