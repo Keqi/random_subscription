01e5a744a13a133f2597d66e2ed89520c7e7a370
Maciej Nowak <maciej.nowak90@gmail.com>
2015-08-25 17:10:36 +0200

    Set routes for subscriptions index and create actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'subscriptions/index'
+  resources :subscriptions, only: [:index, :create]
 
-  get 'subscriptions/create'
+  root 'subscriptions#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
